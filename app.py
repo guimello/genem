25caefa495e9e3be73dd39fde9c303367a2d1feb
guimello <guilhermesilvamello@gmail.com>
2013-07-30 15:52:36 -0300

    Auto complete com busca da cidade

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from models import *
 
 app = Flask(__name__, static_folder="public", static_url_path="")
 connect("genemprod")
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
     return render_template("layout.html", title = "Test App")
+
+@app.route("/find_school", methods=['GET'])
+def find_school():
+    school_name = request.args.get("term").upper()
+    schools = School.objects(name__contains = school_name).order_by("name")
+
+    json_dict = dict(schools = [])
+    for s in schools:
+        json_dict["schools"].append(dict(code=s.code, name=s.name))
+    return jsonify(json_dict)
 
 
 if __name__ == "__main__":
