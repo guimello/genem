5aa86d389e6bcd43e66aa432ef68281173a686d1
guimello <guilhermesilvamello@gmail.com>
2013-07-30 21:57:56 -0300

    Mixin for relative grades

--- models.py
+++ models.py
@@ -1,16 +1,21 @@
 from mongoengine import *
 
-class State(Document):
+class PercentageGrade(object):
+    def relative_grades(self):
+        total = float(sum(self.grades))
+        return [ g / total * 100 for g in self.grades ]
+
+class State(Document, PercentageGrade):
     code = StringField(required = True)
     grades = ListField(IntField())
 
-class City(Document):
+class City(Document, PercentageGrade):
     code = IntField(required = True)
     name = StringField(required = True)
     state_code = StringField(required = True)
     grades = ListField(IntField())
 
-class School(Document):
+class School(Document, PercentageGrade):
     code = IntField(required = True)
     name = StringField(required = True)
     city_code = IntField(required = True)
